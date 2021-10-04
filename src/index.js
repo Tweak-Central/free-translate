eaafcf4991927f868f74f9afc517dc21b9786457
Matix-Media <support@matix-media.net>
2021-10-04 16:31:22 +0200

    Imported compression

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import compression from "compression";
 
 import createRoutes from "./routes/index.js";
 
@@ -14,6 +15,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(compression());
 
 createRoutes(app);
 
