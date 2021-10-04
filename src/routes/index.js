045a00a0fa1682c8062f6512b134780a9d4c4427
Matix-Media <support@matix-media.net>
2021-10-04 23:56:16 +0200

    Added default asset

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,13 @@ module.exports = (app) => {
     // API
     app.use("/api", api);
 
-    // Handle front-end requests
+    // API assets
+    app.use("/assets", express.static("../assets"));
 
+    // Handle front-end requests
     app.use(express.static("../../client/dist"));
 
+    // 500
     app.use((err, req, res, next) => {
         console.error(err.stack);
         res.status(500).json({
