e603a3a887ec6b9e1c9d804df8954a110ee27e21
Matix-Media <support@matix-media.net>
2021-10-05 14:14:50 +0200

    Fixed bug

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,7 +2,7 @@ const morgan = require("morgan");
 const rfs = require("rotating-file-stream");
 const path = require("path");
 
-const devMode = (process.env.NODE_ENV | "development") == "development";
+const devMode = (process.env.NODE_ENV || "development") == "development";
 
 const accessLogStream = rfs.createStream("access.log", {
     interval: "1d",
