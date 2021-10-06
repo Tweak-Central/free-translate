e236dae657ce757bc12bce9b760eb79fa67f48f6
Matix-Media <support@matix-media.net>
2021-10-06 19:26:58 +0200

    Restructured

--- src/routes/api/project/index.js
+++ src/routes/api/project/index.js
@@ -2,12 +2,14 @@ const { Router } = require("express");
 const { isLoggedIn } = require("../../../middleware/user");
 const projectMiddleware = require("../../../middleware/project");
 
+const file = require("./file");
+
 const create = require("./create");
-const uploadFile = require("./uploadFile");
 
 const route = Router();
 
+route.use("/file", file);
+
 route.post("/create", isLoggedIn, projectMiddleware.validateCreation, create);
-route.post("/file/upload", projectMiddleware.validateUserAccess(3), uploadFile);
 
 module.exports = route;
