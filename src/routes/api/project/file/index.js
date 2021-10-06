3ec2e216a9e7a714f46547a1b685140f04989a92
Matix-Media <support@matix-media.net>
2021-10-06 19:37:53 +0200

    Protecting access

--- src/routes/api/project/file/index.js
+++ src/routes/api/project/file/index.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 const projectMiddleware = require("../../../../middleware/project");
 
 const upload = require("./upload");
+const list = require("./list");
 
 const route = Router();
 
 route.post("/upload", projectMiddleware.validateUserAccess(3), upload);
+route.get("/list", projectMiddleware.validateUserAccess(), list);
 
 module.exports = route;
