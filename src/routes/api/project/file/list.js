49d7b36d78d2055db5bb880f4496be1eacbbed51
Matix-Media <support@matix-media.net>
2021-10-06 21:18:43 +0200

    Added functionality

--- src/routes/api/project/file/list.js
+++ src/routes/api/project/file/list.js
@@ -1,3 +1,17 @@
-module.exports = (req, res) => {
-    
-}
+const { models } = require("../../../../models");
+
+module.exports = async (req, res) => {
+    const project = req.project;
+
+    const files = await models.File.findAll({
+        attributes: ["id", "name", "mode"],
+        where: {
+            project: project.get("id"),
+        },
+    });
+
+    res.json({
+        success: true,
+        files: files,
+    });
+};
