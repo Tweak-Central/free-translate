4921dba498d85b5710334ef57d4bdefbc8656164
Matix-Media <support@matix-media.net>
2021-10-06 21:25:55 +0200

    Added translations endpoint

--- src/routes/api/project/file/index.js
+++ src/routes/api/project/file/index.js
@@ -3,10 +3,12 @@ const projectMiddleware = require("../../../../middleware/project");
 
 const upload = require("./upload");
 const list = require("./list");
+const translations = require("./translations");
 
 const route = Router({ mergeParams: true });
 
 route.post("/upload", projectMiddleware.validateUserAccess(3), upload);
 route.get("/list", projectMiddleware.validateUserAccess(), list);
+route.get("/:fileId/translations", translations);
 
 module.exports = route;
