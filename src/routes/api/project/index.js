f80ac7c5a398079af1eb033c1ddfc5262382ee12
Matix-Media <support@matix-media.net>
2021-10-07 16:18:32 +0200

    Added project list endpoint

--- src/routes/api/project/index.js
+++ src/routes/api/project/index.js
@@ -2,14 +2,16 @@ const { Router } = require("express");
 const { isLoggedIn } = require("../../../middleware/user");
 const projectMiddleware = require("../../../middleware/project");
 
-const file = require("./file");
-
 const create = require("./create");
+const list = require("./list");
+
+const file = require("./file");
 
 const route = Router();
 
-route.use("/:projectId/file", file);
-
 route.post("/create", isLoggedIn, projectMiddleware.validateCreation, create);
+route.get("/list", isLoggedIn, list);
+
+route.use("/:projectId/file", file);
 
 module.exports = route;
