71cc66a070889a5020f6f7af96a25c68d6ea4798
Matix-Media <support@matix-media.net>
2021-10-23 18:28:17 +0200

    Added access level check

--- src/middleware/file.js
+++ src/middleware/file.js
@@ -3,23 +3,25 @@ const errors = require("../errors");
 const projectMiddleware = require("./project");
 
 module.exports = {
-    fileExists: [
-        projectMiddleware.validateUserAccess(),
-        async (req, res, next) => {
-            const file = await models.File.findOne({
-                where: {
-                    id: req.params.fileId,
-                },
-            });
-            if (!file)
-                return errors.resError(
-                    res,
-                    errors.getError(404, "The specified project was not found")
-                );
+    fileExists(accessLevel = null) {
+        return [
+            projectMiddleware.validateUserAccess(accessLevel),
+            async (req, res, next) => {
+                const file = await models.File.findOne({
+                    where: {
+                        id: req.params.fileId,
+                    },
+                });
+                if (!file)
+                    return errors.resError(
+                        res,
+                        errors.getError(404, "The specified project was not found")
+                    );
 
-            req.file = file;
+                req.file = file;
 
-            next();
-        },
-    ],
+                next();
+            },
+        ];
+    },
 };
